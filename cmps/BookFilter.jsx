152d3ac850d104dccb2b0a4838acad69bd59d736
NoaLeshem <noalesh20@gmail.com>
2024-11-27 12:03:21 +0200

    added bookFilter component.

---
+++ cmps/BookFilter.jsx
@@ -0,0 +1,9 @@
+
+export function BookFilter({ defaultFilter, onSetFilter }) {
+
+    return (
+        <article className="book-filter">
+           <h1>book filter... under construction...</h1>
+        </article>
+    )
+}
