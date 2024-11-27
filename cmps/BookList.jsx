d2ac0614474759e3098d5d7af4502c77c0cafda3
NoaLeshem <noalesh20@gmail.com>
2024-11-27 22:22:29 +0200

    BookList component Done.

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -1,26 +1,23 @@
+import { BookPreview } from "./BookPreview.jsx";
 
-
+const { Link } = ReactRouterDOM
 
 /* Where the user sees a list of preview items.
     will get a list and render the book previews.
 
 */
 
-import { BookPreview } from "./BookPreview.jsx";
-const { Link } = ReactRouterDOM
-export function BookList({ books, onRemoveBook }) {
+export function BookList({ books }) {
     return (
         <ul className="book-list">
-         {/*   {cars.map(car =>
-                <li key={car.id}>
-                    <CarPreview car={car} />
+           {books.map(book =>
+                <li key={book.id}>
+                    <BookPreview book={book} />
                     <section>
-                        <button onClick={() => onRemoveCar(car.id)}> Remove</button>
-                        <button><Link to={`/car/${car.id}`}>Details</Link></button>
-                        <button ><Link to={`/car/edit/${car.id}`}>Edit</Link></button>
+                        <button><Link to={`/book/${book.id}`}>see Book Details</Link></button>
                     </section>
                 </li>
-            )}   */}
+            )}   
         </ul>   
     )
 
