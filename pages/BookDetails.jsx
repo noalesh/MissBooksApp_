04ea9d3066f2896b45a69bea750ba9ba4c2ee632
NoaLeshem <noalesh20@gmail.com>
2024-11-28 17:47:09 +0200

    BookDetails improvements (not finished)

--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -28,6 +28,46 @@ export function BookDetails() {
     function onBack() {
         navigate('/book')
     }
+
+
+    function calculatePageCount() {
+        const pageCount=book.pageCount
+        if (pageCount>500){
+            return "Serious Reading"
+        } else if (pageCount>200){
+            return "Descent Reading"
+        } else if (pageCount<100){
+            return "Light Reading"
+        } else {
+            return ""
+        }
+    }
+
+    function calculateVintage() {
+        const dateToday = new Date()
+        const yearToday = dateToday.getFullYear()
+        const publishedDate = book.publishedDate
+        if (yearToday-publishedDate>10) {
+            return "Vintage"
+        } else if (yearToday-publishedDate<1) {
+            return "New"
+        } else {
+            return ""
+        }
+    }
+
+    function calculatePriceClassName() {
+        const price = book.listPrice.amount
+        if (price>150) {
+            return "redPrice"
+        } else if (price<20) {
+            return "greenPrice"
+        } else {
+            return "bookDetailsValue"
+        }
+    }
+
+
 /// TODO - what is prevBookId ??? where do we set it ??? ////////
 /// TODO - can I assume that the books' images will be called by their title?
 
@@ -36,15 +76,15 @@ export function BookDetails() {
     return (
         <section className="book-details">
             <h1>Book Details :</h1>
-            <h2><span>Book Title:</span> {book.title}</h2>
-            <h2><span>Book Subtitle:</span> {book.subtitle}</h2>
-            <h2><span>Book Authors:</span> {book.authors}</h2>
-            <h2><span>Book PublishedDate:</span> {book.publishedDate}</h2>
-            <h2><span>Book Description:</span> {book.description}</h2>
-            <h2><span>Book PageCount:</span> {book.pageCount}</h2>
-            <h2><span>Book Categories:</span> {book.categories}</h2>
-            <h2><span>Book Language:</span> {book.language}</h2>
-            <h2><span>Book ListPrice:</span> {book.listPrice.amount}</h2>
+            <h2>Book Title: <span className="bookDetailsValue">{book.title}</span></h2>
+            <h2>Book Subtitle: <span className="bookDetailsValue">{book.subtitle}</span></h2>
+            <h2>Book Authors: <span className="bookDetailsValue">{book.authors}</span></h2>
+            <h2>Book PublishedDate: <span className="bookDetailsValue">{book.publishedDate}</span> <span className="extraInfo">{calculateVintage()}</span></h2>
+            <h2>Book Description: <span className="bookDetailsValue">{book.description}</span></h2>
+            <h2>Book Page Count: <span className="bookDetailsValue">{book.pageCount}</span> <span className="extraInfo">{calculatePageCount()}</span></h2>
+            <h2>Book Categories: <span className="bookDetailsValue">{book.categories}</span></h2>
+            <h2>Book Language: <span className="bookDetailsValue">{book.language}</span></h2>
+            <h2>Book Price: <span className={calculatePriceClassName()}>{book.listPrice.amount}</span></h2>
             <img src={book.thumbnail} alt="book-cover-image" />
             <br></br>
             <button onClick={onBack}>Back</button>
