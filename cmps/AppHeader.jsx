8f7d1436ad789d5cf7b1b388fe4be1878c568daf
NoaLeshem <noalesh20@gmail.com>
2024-12-07 12:50:35 +0200

    changed the logo img path again..  (GitHub problem)

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -6,7 +6,7 @@ export function AppHeader() {
         <header className="app-header-custom full main-layout">
             <section className="header-container">
                 <section className="logo-style">
-                <img src="./assets/logo_img.png" alt="logo image" width="40" height="40"></img>
+                <img src="../assets/logo_img.png" alt="logo image" width="40" height="40"></img>
                 <h1>Miss Books</h1>
                 </section>
             </section>
